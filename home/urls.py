69e631ccce6132e3b182b2af5c3cabeba5812243
Scott Blumenthal <scott@daylife.com>
2011-10-31 11:33:31 -0400

    Adding contact page with signup form for MailChimp.

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('ozet.home.views',
     (r'^history', 'history'),
     (r'^artists', 'artists'),
+    (r'^contact', 'contact'),
     (r'^$', 'index'),
 )
 
--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from ozet.log.models import LogEntry
-from ozet.log.views import get_paginated_log_entries
 
 def index(request):
-    context = get_paginated_log_entries(1)
-    return render_to_response('home/index.html', context, context_instance=RequestContext(request))
+    return render_to_response('home/index.html', context_instance=RequestContext(request))
 
 def artists(request):
     return render_to_response('home/artists.html', context_instance=RequestContext(request))
 
 def history(request):
     return render_to_response('home/history.html', context_instance=RequestContext(request))
+    
+def contact(request):
+    return render_to_response('home/contact.html', context_instance=RequestContext(request))
